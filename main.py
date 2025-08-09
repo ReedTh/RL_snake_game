d531870c957b2ec4d38da2eace8ed3d2678bf45b
ReedTh <reedthompson427@yahoo.com>
2025-08-09 02:02:52 +0100

    Added better comments

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ from enum import Enum
 import numpy as np
 
 pygame.init()
-font = pygame.font.Font('arial.ttf', 25)
+font = pygame.font.Font('arial.ttf', 25) # font for score
 
 class Direction(Enum):
     RIGHT = 1 
     LEFT = 2 
     UP = 3
-    DOWN = 4
+    DOWN = 4 # just directions
 
 
-Point = namedtuple('Point', 'x, y')
+Point = namedtuple('Point', 'x, y') # easy way to store points
 
 
-# rgb colors 
-WHITE = (255, 255, 255 )
+
+# rgb colors
+WHITE = (255, 255, 255)
 RED = (200, 0, 0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0, 0, 0)
 
-BLOCK_SIZE = 20
-SPEED = 30
+BLOCK_SIZE = 20 # size of each block
+SPEED = 30 # game speed
+
 
 
 class SnakeGameAI:
@@ -34,63 +36,60 @@ class SnakeGameAI:
         self.w = w
         self.h = h
         # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
+        self.display = pygame.display.set_mode((self.w, self.h)) # make the window
         pygame.display.set_caption('Snake')
-        self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock() # for controlling speed
         self.reset()
 
 
     def reset(self):
         # init the game state
-        self.direction = Direction.RIGHT
+        self.direction = Direction.RIGHT # always start going right
 
-        self.head = Point(self.w / 2, self.h / 2)
+        self.head = Point(self.w / 2, self.h / 2) # start in the middle
         self.snake = [self.head,
                     Point(self.head.x - BLOCK_SIZE, self.head.y),
-                    Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
-        
+                    Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)] # snake body
         self.score = 0
         self.food = None
-        self._place_food()
-        self.frameIteration = 0
+        self._place_food() # drop the first food
+        self.frameIteration = 0 # how many frames since last food
 
 
     def _place_food(self):
-        x = random.randint(0, (self.w - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
-        y = random.randint(0, (self.h - BLOCK_SIZE) / BLOCK_SIZE) * BLOCK_SIZE
+        # randomly place food somewhere not on the snake
+        x = random.randint(0, int((self.w - BLOCK_SIZE) / BLOCK_SIZE)) * BLOCK_SIZE
+        y = random.randint(0, int((self.h - BLOCK_SIZE) / BLOCK_SIZE)) * BLOCK_SIZE
         self.food = Point(x, y)
         if self.food in self.snake:
             self._place_food()
 
 
     def play_step(self, action):
-        self.frameIteration += 1 
+        self.frameIteration += 1 # count frames
         # check for quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        
         # move
         self._move(action) # updates the head
         self.snake.insert(0, self.head)
 
         # check if game is over
-        reward = 0 
+        reward = 0
         game_over = False
         if self.is_collision() or self.frameIteration > 100*len(self.snake):
             game_over = True
-            reward = -10
+            reward = -10 # bad move
             return reward, game_over, self.score
-        
-        # place new food or just move 
+        # place new food or just move
         if self.head == self.food:
-            self.score += 1 
-            reward = 10
+            self.score += 1
+            reward = 10 # yum
             self._place_food()
         else:
             self.snake.pop()
-        
         # update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
@@ -107,51 +106,44 @@ class SnakeGameAI:
         if pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
-        if pt in self.snake[1]:
+        if pt in self.snake[1:]:
             return True
-        
         return False
 
 
     def _update_ui(self):
-        self.display.fill(BLACK)
-
+        self.display.fill(BLACK) # clear screen
         for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
-
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
-
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE)) # snake body
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12)) # snake border
+        if self.food is not None:
+            pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE)) # food
         text = font.render("Score: " + str(self.score), True, WHITE)
-        self.display.blit(text, [0, 0])
-        pygame.display.flip()
+        self.display.blit(text, [0, 0]) # draw score
+        pygame.display.flip() # update
 
 
     def _move(self, action):
         # [straight, rightTurn, leftTurn]
-        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
         idx = clock_wise.index(self.direction)
-
         if np.array_equal(action, [1,0,0]):
             new_dir = clock_wise[idx] # no change
         elif np.array_equal(action, [0,1,0]):
-            next_idx = (idx + 1) % 4 
+            next_idx = (idx + 1) % 4
             new_dir = clock_wise[next_idx] # right turn
         elif np.array_equal(action, [0,0,1]):
-            next_idx = (idx - 1) % 4 
+            next_idx = (idx - 1) % 4
             new_dir = clock_wise[next_idx] # left turn
-        
         self.direction = new_dir
-
         x = self.head.x
         y = self.head.y
         if self.direction == Direction.RIGHT:
             x += BLOCK_SIZE
-        elif self.direciton == Direction.LEFT:
+        elif self.direction == Direction.LEFT:
             x -= BLOCK_SIZE
         elif self.direction == Direction.UP:
-            y += BLOCK_SIZE
-        elif self.direction == Direction.DOWN:
             y -= BLOCK_SIZE
-        
+        elif self.direction == Direction.DOWN:
+            y += BLOCK_SIZE
         self.head = Point(x, y)
--- model.py
+++ model.py
@@ -9,21 +9,20 @@ import os
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.linear1 = nn.Linear(input_size, hidden_size) # first layer
+        self.linear2 = nn.Linear(hidden_size, output_size) # output layer
 
     def forward(self, x):
-        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear1(x)) # relu for some non-linearity
         x = self.linear2(x)
-        return x 
-    
+        return x
+
     def save(self, file_name='model.pth'):
         model_folder_path = './model'
         if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
-        
+            os.makedirs(model_folder_path) # make folder if it doesn't exist
         file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
+        torch.save(self.state_dict(), file_name) # save the weights
 
 
 class QTrainer:
@@ -31,17 +30,17 @@ class QTrainer:
         self.lr = lr
         self.gamma = gamma
         self.model = model
-        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-        self.critertion = nn.MSELoss()
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr) # adam optimizer
+        self.criterion = nn.MSELoss() # mean squared error
 
     def train_step(self, state, action, reward, next_state, gameOver):
+        # convert everything to tensors just in case
         state = np.array(state, dtype=float)
         state = torch.tensor(state, dtype=torch.float)
         next_state = np.array(next_state, dtype=float)
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float)
-
 
         if len(state.shape) == 1:
             state = torch.unsqueeze(state, 0)
@@ -50,19 +49,16 @@ class QTrainer:
             reward = torch.unsqueeze(reward, 0)
             gameOver = (gameOver, 0)
 
-            # predict Q values with the current state
+        # predict Q values with the current state
         pred = self.model(state)
-
         target = pred.clone()
         for i in range(len(gameOver)):
             newQ = reward[i]
             if not gameOver[i]:
-                newQ = reward[i] + self.gamma * torch.max(self.model(next_state[i]))
-                
-            target[i][torch.argmax(action[i]).item()] = newQ
+                newQ = reward[i] + self.gamma * torch.max(self.model(next_state[i])) # bellman equation
+            target[i][torch.argmax(action[i]).item()] = newQ # update only the action taken
 
         self.optimizer.zero_grad()
-        loss = self.criterion(target, pred)
+        loss = self.criterion(target, pred) # how far off were we?
         loss.backward()
-
-        self.optimizer.step()
+        self.optimizer.step() # do the update
